91560dc32211bb2b1d1123f16e69e4249c80b3bc
STEPHANO MORALES <stephano_morales@hotmail.com>
2025-06-30 15:56:25 +1000

    Cleaned up gitignore and removed local-only files

--- config/auth.py
+++ config/auth.py
@@ -1,2 +1,5 @@
+from pathlib import Path
 
-auth = r"D:\DocumentsSte\VSCode\Proj1Streamlit\resources\car_prices.csv"
+current_folder = Path(__file__).resolve().parent
+project_root = current_folder.parent  # Go one level up
+auth = project_root / "resources" / "car_prices.csv"
